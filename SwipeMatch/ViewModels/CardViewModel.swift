c76eded20556d4ac1ebdc889d1794b1d0cf2122d
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-06-27 17:13:37 +0200

    First commit

---
+++ SwipeMatch/ViewModels/CardViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardViewModel.swift
+//  SwipeMatch
+//
+//  Created by Bochkarov Valentyn on 15/05/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+    // we`ll define the properties that are view will display/render out
+    let imageName: String
+    let attributedString: NSAttributedString
+    let textAlignment: NSTextAlignment
+}
---
+++ SwipeMatch/Views/CardView.swift
@@ -0,0 +1,99 @@
+//
+//  CardView.swift
+//  SwipeMatch
+//
+//  Created by Bochkarov Valentyn on 10/05/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
+    let informationLabel = UILabel()
+    
+    
+    //Configuration
+    fileprivate let threshold: CGFloat = 80
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        imageView.contentMode = .scaleAspectFill
+        addSubview(imageView)
+        imageView.fillSuperview()
+        
+        addSubview(informationLabel)
+        informationLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
+        
+        informationLabel.text = "TEST NAME TEST NAME AGE"
+        informationLabel.textColor = .white
+        informationLabel.font = UIFont.systemFont(ofSize: 34, weight: .heavy)
+        informationLabel.numberOfLines = 0
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        addGestureRecognizer(panGesture)
+    }
+    
+
+    
+
+    
+    @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
+        
+        
+        switch gesture.state {
+        case .changed:
+            handleChanged(gesture)
+        case .ended:
+            handleEnded(gesture: gesture)
+        default:
+            ()
+        }
+    }
+    
+    fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: nil)
+        // rotation
+        
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
+        
+    }
+    fileprivate func handleEnded(gesture: UIPanGestureRecognizer) {
+        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+            if shouldDismissCard {
+                
+//                self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+                self.center = CGPoint(x: 600 * translationDirection, y: 0)
+
+            } else {
+                self.transform = .identity
+            }
+            
+        }) { (_) in
+            self.transform = .identity
+            if shouldDismissCard {
+                self.removeFromSuperview()
+            }
+            
+//            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
